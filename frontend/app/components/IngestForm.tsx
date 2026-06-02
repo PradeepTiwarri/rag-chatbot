f4855a317aa7d22e462b2175118eff75bc1107e9
PradeepTiwarri <pradeeptiwarii731@gmail.com>
2026-06-03 00:02:24 +0530

    github bug not updating pushed files

--- frontend/app/components/IngestForm.tsx
+++ frontend/app/components/IngestForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { API_BASE_URL } from "../config";
+//import { API_BASE_URL } from "../config";
 
 import { useState } from "react";
 import { IngestResponse, VideoMetadata } from "../types";
