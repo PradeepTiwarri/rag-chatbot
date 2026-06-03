8ce5837e2a3a291b2c9b4c318acd1850f9f97123
PradeepTiwarri <pradeeptiwarii731@gmail.com>
2026-06-03 17:36:40 +0530

    made changes in the response from backend

--- frontend/app/components/IngestForm.tsx
+++ frontend/app/components/IngestForm.tsx
@@ -59,17 +59,16 @@ export default function IngestForm({ onIngestComplete }: IngestFormProps) {
       if (
         data.A.status === "success" &&
         data.B.status === "success" &&
-        data.A.metadata &&
-        data.B.metadata
+        
       ) {
         setPipelineStep("done");
-        setTimeout(() => {
-          onIngestComplete(data.A.metadata!, data.B.metadata!);
-        }, 600);
+        // setTimeout(() => {
+        //   onIngestComplete(data.A.metadata!, data.B.metadata!);
+        // }, 600);
       } else {
         const errorMsg =
           data.A.error || data.B.error || "Unknown error during ingestion";
-        alert(`Ingestion error: ${errorMsg}`);
+        console.log(`Ingestion error: ${errorMsg}`);
         setPipelineStep("idle");
       }
     } catch (error) {
